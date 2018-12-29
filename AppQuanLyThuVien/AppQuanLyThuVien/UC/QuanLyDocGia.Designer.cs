1b4af9f036fa4e5ddd43e39db1ef543ac344cf3e
hoho <thienho1998dragon@gmail.com>
2018-12-29 18:51:08 +0700

    Thử nghiệm

--- AppQuanLyThuVien/AppQuanLyThuVien/UC/QuanLyDocGia.Designer.cs
+++ AppQuanLyThuVien/AppQuanLyThuVien/UC/QuanLyDocGia.Designer.cs
@@ -41,18 +41,20 @@
             this.panel15.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.panel15.Controls.Add(this.pictureBox1);
             this.panel15.Controls.Add(this.label1);
-            this.panel15.Location = new System.Drawing.Point(0, -1);
+            this.panel15.Location = new System.Drawing.Point(0, -2);
+            this.panel15.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel15.Name = "panel15";
-            this.panel15.Size = new System.Drawing.Size(850, 46);
+            this.panel15.Size = new System.Drawing.Size(1275, 71);
             this.panel15.TabIndex = 8;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(246, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(369, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(46, 46);
+            this.pictureBox1.Size = new System.Drawing.Size(69, 71);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
@@ -60,20 +62,22 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(18, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(236, 29);
+            this.label1.Size = new System.Drawing.Size(350, 40);
             this.label1.TabIndex = 0;
             this.label1.Text = "QUẢN LÝ ĐỘC GIẢ";
             // 
             // QuanLyDocGia
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.Controls.Add(this.panel15);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "QuanLyDocGia";
-            this.Size = new System.Drawing.Size(850, 500);
+            this.Size = new System.Drawing.Size(1275, 769);
             this.Load += new System.EventHandler(this.QuanLyDocGia_Load);
             this.panel15.ResumeLayout(false);
             this.panel15.PerformLayout();
